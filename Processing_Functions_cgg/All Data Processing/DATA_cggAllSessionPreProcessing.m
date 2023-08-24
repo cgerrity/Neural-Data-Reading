42514fa9ebe348265dcfed40ec256fd1dc4d1cb8
gerritcg <charles.g.gerrity@vanderbilt.edu>
2023-08-24 14:53:42 -0500

    Full Data processing and preprocessing

---
+++ Processing_Functions_cgg/All Data Processing/DATA_cggAllSessionPreProcessing.m
@@ -0,0 +1,340 @@
+%% DATA_cggAllSessionPreProcessing
+
+%% Wo_Probe_01_23-02-13_003_01
+clc; clear; close all;
+
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-13_003_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Wo_Probe_01_23-02-21_006_01
+clc; clear; close all;
+
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-21_006_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Wo_Probe_01_23-02-22_007_01
+clc; clear; close all;
+
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-22_007_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Wo_Probe_01_23-02-23_008_01
+clc; clear; close all;
+
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-23_008_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Wo_Probe_01_23-02-24_009_01
+clc; clear; close all;
+
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-24_009_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Wo_Probe_01_23-02-27_010_01
+clc; clear; close all;
+
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-27_010_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Wo_Probe_01_23-02-28_011_01
+clc; clear; close all;
+
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-28_011_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Wo_Probe_01_23-03-01_012_01
+clc; clear; close all;
+
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-03-01_012_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Wo_Probe_01_23-03-02_013_01
+clc; clear; close all;
+
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-03-02_013_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Wo_Probe_01_23-03-03_014_01
+clc; clear; close all;
+
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-03-03_014_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Wo_Probe_01_23-03-07_016_01
+clc; clear; close all;
+
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-03-07_016_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Wo_Probe_01_23-03-08_017_01
+clc; clear; close all;
+
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-03-08_017_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Wo_Probe_01_23-03-09_018_01
+clc; clear; close all;
+
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-03-09_018_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+% %% Fr_Probe_02_22-04-27_003_01
+% clc; clear; close all;
+% 
+% Monkey_Name='Frey';
+% ExperimentName='Frey_FLToken_Probe_02';
+% SessionName='Fr_Probe_02_22-04-27_003_01';
+% 
+% [inputfolder,outdatadir,~] = ...
+%     cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+%     ExperimentName,SessionName);
+% 
+% cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+%     'outdatadir',outdatadir);
+%% Fr_Probe_02_22-05-02_004_01
+clc; clear; close all;
+
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-02_004_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Fr_Probe_02_22-05-03_005_01
+clc; clear; close all;
+
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-03_005_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Fr_Probe_02_22-05-04_006_01
+clc; clear; close all;
+
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-04_006_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Fr_Probe_02_22-05-05_007_01
+clc; clear; close all;
+
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-05_007_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Fr_Probe_02_22-05-06_008_01
+clc; clear; close all;
+
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-06_008_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Fr_Probe_02_22-05-09_009_01
+clc; clear; close all;
+
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-09_009_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Fr_Probe_02_22-05-10_010_01
+clc; clear; close all;
+
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-10_010_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Fr_Probe_02_22-05-11_011_01
+clc; clear; close all;
+
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-11_011_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Fr_Probe_03_22-06-30_001_02
+clc; clear; close all;
+
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_03';
+SessionName='Fr_Probe_03_22-06-30_001_02';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Fr_Probe_03_22-07-13_002_01
+clc; clear; close all;
+
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_03';
+SessionName='Fr_Probe_03_22-07-13_002_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Fr_Probe_03_22-07-15_003_01
+clc; clear; close all;
+
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_03';
+SessionName='Fr_Probe_03_22-07-15_003_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
+%% Fr_Probe_03_22-07-21_004_01
+clc; clear; close all;
+
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_03';
+SessionName='Fr_Probe_03_22-07-21_004_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+cgg_proc_NeuralDataPreparation('inputfolder',inputfolder,...
+    'outdatadir',outdatadir);
---
+++ Processing_Functions_cgg/All Data Processing/DATA_cggAllSessionProcessing.m
@@ -0,0 +1,17 @@
+%% DATA_cggAllSessionProcessing
+
+clc; clear; close all;
+
+[cfg] = DATA_cggAllSessionInformationConfiguration;
+
+%%
+
+for sidx=1:length(cfg)
+    
+    inputfolder=cfg(sidx).inputfolder;
+    outdatadir=cfg(sidx).outdatadir;
+
+cgg_procFullTrialPreparation_v3(...
+    'inputfolder',cfg(sidx).inputfolder,'outdatadir',cfg(sidx).outdatadir);
+
+end
