020c31635864a701802d935df838d409cf681e9e
gerritcg <charles.g.gerrity@vanderbilt.edu>
2023-08-24 14:53:13 -0500

    function to get information on each dataset

---
+++ Processing_Functions_cgg/All Data Processing/DATA_cggAllSessionInformationConfiguration.m
@@ -0,0 +1,475 @@
+function [cfg] = DATA_cggAllSessionInformationConfiguration
+%DATA_CGGALLSESSIONINFORMATIONCONFIGURATION Summary of this function goes here
+%   Detailed explanation goes here
+
+cfg=struct([]);
+%% Wo_Probe_01_23-02-13_003_01
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-13_003_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Wo_Probe_01_23-02-21_006_01
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-21_006_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Wo_Probe_01_23-02-22_007_01
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-22_007_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Wo_Probe_01_23-02-23_008_01
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-23_008_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Wo_Probe_01_23-02-24_009_01
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-24_009_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Wo_Probe_01_23-02-27_010_01
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-27_010_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Wo_Probe_01_23-02-28_011_01
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-02-28_011_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Wo_Probe_01_23-03-01_012_01
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-03-01_012_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Wo_Probe_01_23-03-02_013_01
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-03-02_013_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Wo_Probe_01_23-03-03_014_01
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-03-03_014_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Wo_Probe_01_23-03-07_016_01
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-03-07_016_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Wo_Probe_01_23-03-08_017_01
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-03-08_017_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Wo_Probe_01_23-03-09_018_01
+Monkey_Name='Wotan';
+ExperimentName='Wotan_FLToken_Probe_01';
+SessionName='Wo_Probe_01_23-03-09_018_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+% %% Fr_Probe_02_22-04-27_003_01
+% Monkey_Name='Frey';
+% ExperimentName='Frey_FLToken_Probe_02';
+% SessionName='Fr_Probe_02_22-04-27_003_01';
+% 
+% [inputfolder,outdatadir,~] = ...
+%     cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+%     ExperimentName,SessionName);
+% 
+% this_FieldNum=length(cfg)+1;
+% cfg(this_FieldNum).inputfolder=inputfolder;
+% cfg(this_FieldNum).outdatadir=outdatadir;
+% cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+% cfg(this_FieldNum).ExperimentName=ExperimentName;
+% cfg(this_FieldNum).SessionName=SessionName;
+% cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+%     filesep SessionName];
+% 
+%% Fr_Probe_02_22-05-02_004_01
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-02_004_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Fr_Probe_02_22-05-03_005_01
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-03_005_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Fr_Probe_02_22-05-04_006_01
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-04_006_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Fr_Probe_02_22-05-05_007_01
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-05_007_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Fr_Probe_02_22-05-06_008_01
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-06_008_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Fr_Probe_02_22-05-09_009_01
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-09_009_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Fr_Probe_02_22-05-10_010_01
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-10_010_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Fr_Probe_02_22-05-11_011_01
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_02';
+SessionName='Fr_Probe_02_22-05-11_011_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Fr_Probe_03_22-06-30_001_02
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_03';
+SessionName='Fr_Probe_03_22-06-30_001_02';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Fr_Probe_03_22-07-13_002_01
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_03';
+SessionName='Fr_Probe_03_22-07-13_002_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Fr_Probe_03_22-07-15_003_01
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_03';
+SessionName='Fr_Probe_03_22-07-15_003_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+%% Fr_Probe_03_22-07-21_004_01
+Monkey_Name='Frey';
+ExperimentName='Frey_FLToken_Probe_03';
+SessionName='Fr_Probe_03_22-07-21_004_01';
+
+[inputfolder,outdatadir,~] = ...
+    cgg_getBaseFoldersFromSessionInformation(Monkey_Name,...
+    ExperimentName,SessionName);
+
+this_FieldNum=length(cfg)+1;
+cfg(this_FieldNum).inputfolder=inputfolder;
+cfg(this_FieldNum).outdatadir=outdatadir;
+cfg(this_FieldNum).Monkey_Name=Monkey_Name;
+cfg(this_FieldNum).ExperimentName=ExperimentName;
+cfg(this_FieldNum).SessionName=SessionName;
+cfg(this_FieldNum).SessionFolder=[outdatadir filesep ExperimentName ...
+    filesep SessionName];
+
+end
+
