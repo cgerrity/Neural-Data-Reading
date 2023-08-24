0264d52a66abfcdb6d829b57328c389774125970
gerritcg <charles.g.gerrity@vanderbilt.edu>
2023-08-24 14:53:42 -0500

    Full Data processing and preprocessing

---
+++ Processing_Functions_cgg/All Data Processing/DATA_cggAllSessionPreProcessing_v2.m
@@ -0,0 +1,17 @@
+%% DATA_cggAllSessionPreProcessing
+
+clc; clear; close all;
+
+[cfg] = DATA_cggAllSessionInformationConfiguration;
+
+%%
+
+for sidx=1:length(cfg)
+    
+    inputfolder=cfg(sidx).inputfolder;
+    outdatadir=cfg(sidx).outdatadir;
+
+[Output,Area_Names] = cgg_proc_NeuralDataPreparation(...
+    'inputfolder',cfg(sidx).inputfolder,'outdatadir',cfg(sidx).outdatadir);
+
+end
