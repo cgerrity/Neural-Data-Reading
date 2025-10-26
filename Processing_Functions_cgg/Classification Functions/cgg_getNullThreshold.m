788b9922efd558217f21cb9f9f22a12a37cc1b12
gerritcg <charles.g.gerrity@vanderbilt.edu>
2025-10-26 15:23:15 -0700

    Update with new importance analysis functions

--- Processing_Functions_cgg/Classification Functions/cgg_getNullThreshold.m
+++ Processing_Functions_cgg/Classification Functions/cgg_getNullThreshold.m
@@ -1,33 +1,25 @@
-function ThresholdTable = cgg_getNullThreshold(EpochDir,Folds,cfg_Encoder,varargin)
+function ThresholdTable = cgg_getNullThreshold(IA_PassTable,cfg_Epoch,cfg_Encoder,varargin)
 %CGG_GETNULLTHRESHOLD Summary of this function goes here
 %   Detailed explanation goes here
 
 
 isfunction=exist('varargin','var');
 
-if isfunction
-MatchType = CheckVararginPairs('MatchType', 'Scaled-BalancedAccuracy', varargin{:});
-else
-if ~(exist('MatchType','var'))
-MatchType='Scaled-BalancedAccuracy';
-end
-end
+% if isfunction
+% MatchType = CheckVararginPairs('MatchType', 'Scaled-BalancedAccuracy', varargin{:});
+% else
+% if ~(exist('MatchType','var'))
+% MatchType='Scaled-BalancedAccuracy';
+% end
+% end
 
-if isfunction
-IsQuaddle = CheckVararginPairs('IsQuaddle', true, varargin{:});
-else
-if ~(exist('IsQuaddle','var'))
-IsQuaddle=true;
-end
-end
-
-if isfunction
-SessionName = CheckVararginPairs('SessionName', 'Subset', varargin{:});
-else
-if ~(exist('SessionName','var'))
-SessionName='Subset';
-end
-end
+% if isfunction
+% SessionName = CheckVararginPairs('SessionName', 'Subset', varargin{:});
+% else
+% if ~(exist('SessionName','var'))
+% SessionName='Subset';
+% end
+% end
 
 if isfunction
 SetType = CheckVararginPairs('SetType', 'Testing', varargin{:});
@@ -46,36 +38,97 @@ end
 end
 
 if isfunction
-TrialFilter = CheckVararginPairs('TrialFilter', 'All', varargin{:});
+EncoderParameters_CM_Table = CheckVararginPairs('EncoderParameters_CM_Table', [], varargin{:});
 else
-if ~(exist('TrialFilter','var'))
-TrialFilter='All';
+if ~(exist('EncoderParameters_CM_Table','var'))
+EncoderParameters_CM_Table=[];
 end
 end
 
-if isfunction
-TargetFilter = CheckVararginPairs('TargetFilter', 'Overall', varargin{:});
-else
-if ~(exist('TargetFilter','var'))
-TargetFilter='Overall';
-end
-end
+% if isfunction
+% TrialFilter = CheckVararginPairs('TrialFilter', {'All'}, varargin{:});
+% else
+% if ~(exist('TrialFilter','var'))
+% TrialFilter={'All'};
+% end
+% end
 
-NumFolds = length(Folds);
+% if isfunction
+% TargetFilter = CheckVararginPairs('TargetFilter', 'Overall', varargin{:});
+% else
+% if ~(exist('TargetFilter','var'))
+% TargetFilter='Overall';
+% end
+% end
+%%
+% [Subset,wantSubset] = cgg_verifySubset(SessionName,true);
+% cfg_Encoder.Subset = Subset;
+% cfg_Encoder.wantSubset = wantSubset;
+fprintf('*** Starting Null Threshold Calculations\n');
+%%
+EpochDir_Main = cgg_getDirectory(cfg_Epoch.TargetDir,'Epoch');
+%%
+
+% NumFolds = length(Folds);
 this_Percentile = (1-Alpha)*100; 
 % One tail since only care about if accuracy is higher not lower
 
-TableVariables = [["Fold", "double"]; ...
-    ["Threshold", "double"]];
+%%
+RequiredFields = ["SessionName","TrialFilter", "TrialFilter_Value",...
+    "TargetFilter","MatchType","Fold","Target"];
+ThresholdTable = IA_PassTable(:,RequiredFields);
 
-NumVariables = size(TableVariables,1);
-ThresholdTable = table('Size',[0,NumVariables],... 
-	    'VariableNames', TableVariables(:,1),...
-	    'VariableTypes', TableVariables(:,2));
+for i = 1:width(ThresholdTable)
+    if isnumeric(ThresholdTable{:, i})
+        ThresholdTable{:, i} = fillmissing(ThresholdTable{:, i}, 'constant', Inf);
+    end
+end
+ThresholdTable = unique(ThresholdTable);
+for i = 1:width(ThresholdTable)
+    if isnumeric(ThresholdTable{:, i})
+        ThresholdTable{isinf(ThresholdTable{:, i}), i} = NaN;
+    end
+end
+%%
 
-for fidx = 1:NumFolds
-    Fold = Folds(fidx);
-    [Training,Validation,Testing,~] = cgg_getDatastore(EpochDir.Main,SessionName,Fold,cfg_Encoder);
+% NullTable = ThresholdTable;
+% NullTable.UnScaledChance = cell(height(ThresholdTable),1);
+% NullTable.ChanceDistribution = cell(height(ThresholdTable),1);
+
+%%
+ThresholdTable.Threshold = zeros(height(ThresholdTable),1);
+
+%%
+waitbar = parallel.pool.Constant(cgg_getWaitBar(...
+    'All_Iterations',height(ThresholdTable),'Process','Null Threshold',...
+    'DisplayIndents', 0));
+
+for tidx = 1:height(ThresholdTable)
+    SessionName = ThresholdTable{tidx,"SessionName"};
+    Fold = ThresholdTable{tidx,"Fold"};
+    this_cfg_Encoder = cfg_Encoder;
+    [Subset,wantSubset] = cgg_verifySubset(SessionName,true);
+    this_cfg_Encoder.Subset = Subset;
+    this_cfg_Encoder.wantSubset = wantSubset;
+    Target = ThresholdTable{tidx,"Target"};
+    if ~strcmp(Target,cfg_Encoder.Target)
+        fprintf("!!! Reassigning the Encoder Target to align with Null Table Row\n");
+        this_cfg_Encoder.Target = Target;
+    end
+
+    %%
+    if ~isempty(EncoderParameters_CM_Table)
+    EncoderParameter_TargetIDX = strcmp(EncoderParameters_CM_Table.Target,Target);
+    EncoderParameter_SubsetIDX = strcmp(EncoderParameters_CM_Table.Subset,SessionName);
+    this_Row = EncoderParameter_TargetIDX & EncoderParameter_SubsetIDX;
+    this_EncoderParameter_CM_Table = EncoderParameters_CM_Table(this_Row,:);
+    this_CM_TableIDX = this_EncoderParameter_CM_Table.Fold{1} == Fold;
+    this_CM_Table = this_EncoderParameter_CM_Table.CM_Table{1}{this_CM_TableIDX};
+    DataNumber = this_CM_Table.DataNumber;
+    TrueValue = this_CM_Table.TrueValue;
+    %%
+    else
+    [Training,Validation,Testing,~] = cgg_getDatastore(EpochDir_Main,SessionName,Fold,this_cfg_Encoder,'WantData',false);
     
     switch SetType
         case 'Training'
@@ -88,13 +141,81 @@ for fidx = 1:NumFolds
             DataStore = Testing;
     end
 
-    [ClassNames,~,~,~,TrueValue] = cgg_getClassesFromDataStore(DataStore);
-    [Distribution] = cgg_getBaselineAccuracyDistribution(TrueValue,ClassNames,MatchType,IsQuaddle,varargin{:});
-    
-    this_Threshold = prctile(Distribution(1,:),this_Percentile);
+    this_Output = readall(DataStore,UseParallel=true);
+    DataNumber = [this_Output{:,2}]';
+    TrueValue = [this_Output{:,1}]';
 
-    ThresholdTable(Fold,:) = {Fold,this_Threshold};
+    end
+CM_Table = cgg_generateBlankCMTable('DataNumber',DataNumber,'TrueValue',TrueValue);
+%%
+
+TrialFilter = ThresholdTable{tidx,"TrialFilter"};
+TrialFilter_Value = ThresholdTable{tidx,"TrialFilter_Value"};
+MatchType = ThresholdTable{tidx,"MatchType"};
+% AttentionalFilter = ThresholdTable{tidx,"TargetFilter"};
+TargetFilter = ThresholdTable{tidx,"TargetFilter"};
+
+[IsComplete,NullTable] = cgg_isNullTableComplete(CM_Table,cfg_Epoch,...
+    this_cfg_Encoder,'TrialFilter',TrialFilter,...
+    'TrialFilter_Value',TrialFilter_Value,...
+    'MatchType',MatchType,'TargetFilter',TargetFilter);
+
+% this_Counter = 0;
+while ~IsComplete
+    % this_Counter = this_Counter + 1;
+    % fprintf("??? ~IsComplete %d; Counter: %d\n",~IsComplete,this_Counter);
+    cgg_getNullTable(CM_Table,cfg_Epoch,this_cfg_Encoder,...
+        'TrialFilter',TrialFilter,'TrialFilter_Value',TrialFilter_Value,...
+        'MatchType',MatchType,'TargetFilter',TargetFilter);
+    pause(60);
+    [IsComplete,NullTable] = cgg_isNullTableComplete(CM_Table,cfg_Epoch,...
+        this_cfg_Encoder,'TrialFilter',TrialFilter,...
+        'TrialFilter_Value',TrialFilter_Value,...
+        'MatchType',MatchType,'TargetFilter',TargetFilter);
 end
+
+DataNumber_Null = NullTable.DataNumber;
+this_DataNumber = CM_Table.DataNumber;
+MatchingNullEntry = cellfun(@(x) isequal(sort(x),sort(this_DataNumber)),DataNumber_Null,'UniformOutput',true);
+
+this_NullTable = NullTable(MatchingNullEntry,:);
+ChanceDistribution = this_NullTable.ChanceDistribution{1};
+this_Threshold = prctile(ChanceDistribution,this_Percentile);
+ThresholdTable(tidx,"Threshold") = {this_Threshold};
+
+waitbar.Value.update();
+end
+
+%%
+
+ThresholdTable{:, "TrialFilter_Value"} = fillmissing(ThresholdTable{:, "TrialFilter_Value"}, 'constant', Inf);
+% IA_PassTable{:, "TrialFilter_Value"} = fillmissing(IA_PassTable{:, "TrialFilter_Value"}, 'constant', Inf);
+% aaa = join(IA_PassTable,ThresholdTable);
+
+% %%
+% 
+% for fidx = 1:NumFolds
+%     Fold = Folds(fidx);
+%     [Training,Validation,Testing,~] = cgg_getDatastore(EpochDir_Main,SessionName,Fold,cfg_Encoder);
+% 
+%     switch SetType
+%         case 'Training'
+%             DataStore = Training;
+%         case 'Validation'
+%             DataStore = Validation;
+%         case 'Testing'
+%             DataStore = Testing;
+%         otherwise
+%             DataStore = Testing;
+%     end
+% 
+%     [ClassNames,~,~,~,TrueValue] = cgg_getClassesFromDataStore(DataStore);
+%     [Distribution] = cgg_getBaselineAccuracyDistribution(TrueValue,ClassNames,MatchType,IsQuaddle,varargin{:});
+% 
+%     this_Threshold = prctile(Distribution(1,:),this_Percentile);
+% 
+%     ThresholdTable(Fold,:) = {Fold,this_Threshold};
+% end
 
 end
 
---
+++ Processing_Functions_cgg/Importance Analysis/cgg_runBlockImportanceAnalysis.m
@@ -0,0 +1,169 @@
+function IA_Table = cgg_runBlockImportanceAnalysis(PassTableEntry,...
+    cfg_Epoch,cfg_Encoder)
+%CGG_RUNBLOCKIMPORTANCEANALYSIS Summary of this function goes here
+%   Detailed explanation goes here
+
+% <<<<<<BLOCK>>>>>>
+fprintf('   *** Starting Block Run\n');
+[IAPathName,RemovalTablePathName,IATestPathName] = ...
+    cgg_generateImportanceAnalysisFileNames(PassTableEntry,cfg_Epoch,...
+    'WantDirectory',true);
+RemovalTablePathNameExt = sprintf('%s.mat',RemovalTablePathName);
+IATestPathNameExt = sprintf('%s.mat',IATestPathName);
+IAPathNameNameExt = sprintf('%s.mat',IAPathName);
+
+%%
+Message_FileIA = extractAfter(fileparts(IATestPathNameExt),...
+    'Importance Analysis/');
+fprintf('   *** File: %s\n',Message_FileIA);
+%%
+
+if isfield(cfg_Encoder,'Epoch')
+Epoch = cfg_Encoder.Epoch;
+end
+
+PassTableVariableNames = PassTableEntry.Properties.VariableNames;
+if ismember("SessionName", PassTableVariableNames)
+SessionName = PassTableEntry.SessionName;
+end
+if ismember("Fold", PassTableVariableNames)
+Fold = PassTableEntry.Fold;
+end
+if ismember("RemovalType", PassTableVariableNames)
+RemovalType = PassTableEntry.RemovalType;
+end
+if ismember("TrialFilter", PassTableVariableNames)
+TrialFilter = PassTableEntry.TrialFilter;
+end
+if ismember("TrialFilter_Value", PassTableVariableNames)
+TrialFilter_Value = PassTableEntry.TrialFilter_Value;
+end
+if ismember("MatchType", PassTableVariableNames)
+MatchType = PassTableEntry.MatchType;
+MatchType_Attention = MatchType;
+end
+if ismember("TimeRange", PassTableVariableNames)
+TimeRange = PassTableEntry.TimeRange;
+end
+if ismember("TargetFilter", PassTableVariableNames)
+TargetFilter = PassTableEntry.TargetFilter;
+AttentionalFilter = TargetFilter;
+end
+if ismember("Target", PassTableVariableNames)
+Target = PassTableEntry.Target;
+end
+
+IsQuaddle = false;
+if strcmp(Target,"Dimension")
+    IsQuaddle = true;
+end
+
+%%
+
+% EpochDir_Main = cgg_getDirectory(cfg_Epoch.TargetDir,'Epoch');
+FoldDir = cgg_getDirectory(cfg_Epoch.ResultsDir,'Fold');
+
+[Subset,wantSubset] = cgg_verifySubset(SessionName,cfg_Encoder.wantSubset);
+cfg_Encoder.Subset = Subset;
+cfg_Encoder.wantSubset = wantSubset;
+cfg_Encoder.Fold = Fold;
+%% Get Network and Data
+fprintf('   *** Getting Network and Data\n');
+
+% Network
+cfg_Network = cgg_generateEncoderSubFolders_v2(FoldDir,cfg_Encoder,'WantDirectory',false);
+Encoding_Dir = cgg_getDirectory(cfg_Network,'Classifier');
+EncoderPathNameExt =fullfile(Encoding_Dir, 'Encoder-Optimal.mat');
+ClassifierPathNameExt =fullfile(Encoding_Dir, 'Classifier-Optimal.mat');
+
+HasEncoderClassifier = isfile(EncoderPathNameExt) && isfile(ClassifierPathNameExt);
+
+if HasEncoderClassifier
+    % m_Encoder = matfile(EncoderPathNameExt,"Writable",false);
+    m_Encoder = load(EncoderPathNameExt);
+    Encoder=m_Encoder.Encoder;
+    % m_Classifier = matfile(ClassifierPathNameExt,"Writable",false);
+    m_Classifier = load(ClassifierPathNameExt);
+    Classifier=m_Classifier.Classifier;
+else
+    fprintf('   !!! No Encoder and Classifier\n');
+    return
+end
+
+% Data
+EpochDir_Main = cgg_getDirectory(cfg_Epoch.TargetDir,'Epoch');
+[~,~,Testing,ClassNames,~] = cgg_getDatastore(EpochDir_Main,SessionName,Fold,cfg_Encoder,'WantData',true);
+
+%%
+BadData = preview(Testing);
+BadData = squeeze(isnan(BadData{1}(:,1,:,1)));
+
+[BadChannel,BadArea] = ind2sub(size(BadData),find(BadData));
+BadChannelTable = table(BadChannel,BadArea,'VariableNames',{'ChannelIndices','AreaIndices'});
+
+%
+LayerNameIDX = contains({Encoder.Layers(:).Name},'Input_Encoder');
+InputSize = Encoder.Layers(LayerNameIDX).InputSize;
+LayerNameIDX = contains({Classifier.Layers(:).Name},'Input_Classifier');
+LatentSize = Classifier.Layers(LayerNameIDX).InputSize;
+
+NumChannels = InputSize(1);
+NumAreas = InputSize(3);
+%% Generate Removal Table
+fprintf('   *** Generating Removal Table\n');
+if isfile(RemovalTablePathNameExt)
+    RemovalTable = load(RemovalTablePathNameExt);
+    RemovalTable = RemovalTable.RemovalTable;
+else
+% <<<<<<BLOCK>>>>>>
+    RemovalTable = cgg_getBlockRemovalTable(NumChannels,NumAreas,LatentSize,BadChannelTable,RemovalType);
+    save(RemovalTablePathNameExt,'RemovalTable');
+end
+%% Obtain CM_Table Results
+fprintf('   *** Obtaining IA Test Results\n');
+if isfile(IATestPathNameExt)
+    IA_Table_Test = load(IATestPathNameExt);
+    IA_Table_Test = IA_Table_Test.IA_Table_Test;
+else
+    IA_Table_Test = cgg_procImportanceAnalysisFromRemovalTable(RemovalTable, Testing, Encoder, Classifier, 'ClassNames',ClassNames,'IsQuaddle',IsQuaddle);
+    save(IATestPathNameExt,'IA_Table_Test');
+end
+
+%% Select Most Important Removal
+
+fprintf('   *** Obtaining IA Results\n');
+if isfile(IAPathNameNameExt)
+    IA_Table = load(IAPathNameNameExt);
+    IA_Table = IA_Table.IA_Table;
+else
+    %% Obtain Metric Results
+
+    [~,NullTable] = cgg_isNullTableComplete(IA_Table_Test.CM_Table,cfg_Epoch,cfg_Encoder,'TrialFilter',TrialFilter,'TrialFilter_Value',TrialFilter_Value,...
+        'MatchType',MatchType,'TargetFilter',TargetFilter);
+    Identifiers_Table = cgg_getIdentifiersTable(cfg_Epoch,wantSubset,'Epoch',Epoch,'Subset',Subset);
+
+    MetricFunc = @(x) cgg_procCompleteMetric(x,cfg_Epoch,'Epoch',Epoch,...
+        'TrialFilter',TrialFilter,'TrialFilter_Value',TrialFilter_Value,...
+        'MatchType',MatchType,'IsQuaddle',IsQuaddle,...
+        'MatchType_Attention',MatchType_Attention,'TimeRange',TimeRange,...
+        'AttentionalFilter',AttentionalFilter,'Subset',Subset,...
+        'cfg_Encoder',cfg_Encoder,'Target',Target,...
+        'WantUseNullTable',true,'Identifiers_Table',Identifiers_Table,...
+        'NullTable',NullTable);
+    
+    MetricOutput = rowfun(MetricFunc,IA_Table_Test,"InputVariables","CM_Table","SeparateInputs",true,"ExtractCellContents",true,"NumOutputs",2,"OutputFormat","cell");
+    
+    Metric = cell2mat(MetricOutput(:,1));
+    Window_Metric = cell2mat(MetricOutput(:,2));
+%%
+    % Block Specific
+    % <<<<<<BLOCK>>>>>>
+    IA_Table = IA_Table_Test;
+    IA_Table = removevars(IA_Table, "CM_Table");
+    IA_Table.Metric = Metric;
+    IA_Table.WindowMetric = Window_Metric;
+    save(IAPathNameNameExt,'IA_Table');
+end
+
+end
+
