2ceeae971eb755a77fede3d2627729771dda9153
gerritcg <charles.g.gerrity@vanderbilt.edu>
2025-10-26 15:29:12 -0700

    Update analysis folder generation to use concise and clear directory information

--- Processing_Functions_cgg/Input_Output_Information/cgg_generateAnalysisFolders.m
+++ Processing_Functions_cgg/Input_Output_Information/cgg_generateAnalysisFolders.m
@@ -4,7 +4,7 @@ function cfg = cgg_generateAnalysisFolders(EpochDir,varargin)
 
 cfg=struct();
 
-cfg.EpochDir.path=EpochDir;
+cfg.Epoch.path=EpochDir;
 
 AnalysisType = CheckVararginPairs('AnalysisType', '', varargin{:});
 AnalysisTypeSubField = CheckVararginPairs('AnalysisTypeSubField', '', varargin{:});
@@ -15,42 +15,42 @@ WantDirectory = CheckVararginPairs('WantDirectory', true, varargin{:});
 %% Analysis Folder
 
 % Make the Analysis folder name
-cfg_tmp=cfg.EpochDir;
+cfg_tmp=cfg.Epoch;
 [cfg_tmp,~] = cgg_generateFolderAndPath('Analysis','Analysis',cfg_tmp,'WantDirectory',WantDirectory);
-cfg.EpochDir=cfg_tmp;
+cfg.Epoch=cfg_tmp;
 
 %%
 
 if ~isempty(AnalysisType)
 % Make the Analysis Type folder name
-cfg_tmp=cfg.EpochDir.Analysis;
+cfg_tmp=cfg.Epoch.Analysis;
 [cfg_tmp,~] = cgg_generateFolderAndPath(AnalysisType,'AnalysisType',cfg_tmp,'WantDirectory',WantDirectory);
-cfg.EpochDir.Analysis=cfg_tmp;
+cfg.Epoch.Analysis=cfg_tmp;
 
 if ~isempty(AnalysisTypeSubField)
 % Make the Analysis Type Sub Field folder name
-cfg_tmp=cfg.EpochDir.Analysis.AnalysisType;
+cfg_tmp=cfg.Epoch.Analysis.AnalysisType;
 [cfg_tmp,~] = cgg_generateFolderAndPath(AnalysisTypeSubField,'AnalysisTypeSubField',cfg_tmp,'WantDirectory',WantDirectory);
-cfg.EpochDir.Analysis.AnalysisType=cfg_tmp;
+cfg.Epoch.Analysis.AnalysisType=cfg_tmp;
 
 if ~isempty(Session)
 % Make the Session folder name
-cfg_tmp=cfg.EpochDir.Analysis.AnalysisType.AnalysisTypeSubField;
+cfg_tmp=cfg.Epoch.Analysis.AnalysisType.AnalysisTypeSubField;
 [cfg_tmp,~] = cgg_generateFolderAndPath(Session,'Session',cfg_tmp,'WantDirectory',WantDirectory);
-cfg.EpochDir.Analysis.AnalysisType.AnalysisTypeSubField=cfg_tmp;
+cfg.Epoch.Analysis.AnalysisType.AnalysisTypeSubField=cfg_tmp;
 
 if ~isempty(AnalysisTypeSubSubField)
 % Make the Analysis Type Sub Field folder name
-cfg_tmp=cfg.EpochDir.Analysis.AnalysisType.AnalysisTypeSubField.Session;
+cfg_tmp=cfg.Epoch.Analysis.AnalysisType.AnalysisTypeSubField.Session;
 [cfg_tmp,~] = cgg_generateFolderAndPath(AnalysisTypeSubSubField,'AnalysisTypeSubSubField',cfg_tmp,'WantDirectory',WantDirectory);
-cfg.EpochDir.Analysis.AnalysisType.AnalysisTypeSubField.Session=cfg_tmp;
+cfg.Epoch.Analysis.AnalysisType.AnalysisTypeSubField.Session=cfg_tmp;
 
 if ~isempty(Fold)
 % Make the Fold folder name
 FoldName = sprintf('Fold %d',Fold);
-cfg_tmp=cfg.EpochDir.Analysis.AnalysisType.AnalysisTypeSubField.Session.AnalysisTypeSubSubField;
+cfg_tmp=cfg.Epoch.Analysis.AnalysisType.AnalysisTypeSubField.Session.AnalysisTypeSubSubField;
 [cfg_tmp,~] = cgg_generateFolderAndPath(FoldName,'Fold',cfg_tmp,'WantDirectory',WantDirectory);
-cfg.EpochDir.Analysis.AnalysisType.AnalysisTypeSubField.Session.AnalysisTypeSubSubField=cfg_tmp;
+cfg.Epoch.Analysis.AnalysisType.AnalysisTypeSubField.Session.AnalysisTypeSubSubField=cfg_tmp;
 
 end % End for whether to make the Fold Folder
 end % End for whether to make the Analysis Type Sub Sub Field Folder
@@ -59,16 +59,16 @@ end % End for whether to make the Analysis Type Sub Field Folder
 
 if ~isempty(Session) && isempty(AnalysisTypeSubField)
 % Make the Session folder name
-cfg_tmp=cfg.EpochDir.Analysis.AnalysisType;
+cfg_tmp=cfg.Epoch.Analysis.AnalysisType;
 [cfg_tmp,~] = cgg_generateFolderAndPath(Session,'Session',cfg_tmp,'WantDirectory',WantDirectory);
-cfg.EpochDir.Analysis.AnalysisType=cfg_tmp;
+cfg.Epoch.Analysis.AnalysisType=cfg_tmp;
 
 if ~isempty(Fold)
 % Make the Fold folder name
 FoldName = sprintf('Fold %d',Fold);
-cfg_tmp=cfg.EpochDir.Analysis.AnalysisType.Session;
+cfg_tmp=cfg.Epoch.Analysis.AnalysisType.Session;
 [cfg_tmp,~] = cgg_generateFolderAndPath(FoldName,'Fold',cfg_tmp,'WantDirectory',WantDirectory);
-cfg.EpochDir.Analysis.AnalysisType.Session=cfg_tmp;
+cfg.Epoch.Analysis.AnalysisType.Session=cfg_tmp;
 
 end % End for whether to make the Fold Folder
 end % End for whether to make the Session Folder
