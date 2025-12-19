46a021718fb7eeaa3f6532be2ae43668919f6a34
gerritcg <charles.g.gerrity@vanderbilt.edu>
2025-12-19 11:55:51 -0800

    Update to Block Importance analysis

--- Processing_Functions_cgg/Importance Analysis/cgg_addBlockImportanceAnalysisToAccuracyTable.m
+++ Processing_Functions_cgg/Importance Analysis/cgg_addBlockImportanceAnalysisToAccuracyTable.m
@@ -13,6 +13,14 @@ TableType='Full';
 end
 end
 
+if isfunction
+Areas = CheckVararginPairs('Areas', [], varargin{:});
+else
+if ~(exist('Areas','var'))
+Areas=[];
+end
+end
+
 %%
 switch TableType
     case 'Full'
@@ -20,9 +28,15 @@ switch TableType
     case 'Split'
         TrialFilter = PassTable.TrialFilter;
         TrialFilter_Value = PassTable.TrialFilter_Value;
+        % [TrialFilter,TrialFilter_Value] = cgg_getPackedTrialFilter(TrialFilter,TrialFilter_Value,'Unpack');
+        UnPackFunction = @(TF_var,TFV_var) cgg_getPackedTrialFilter(TF_var,TFV_var,'Unpack');
         RowNames = arrayfun(@(x,y) cgg_getSplitTableRowNames(...
-            TrialFilter(x,:),y),(1:size(TrialFilter,1))',...
-            TrialFilter_Value);
+            cgg_getOutput({x,y},UnPackFunction,1,'MultiInput',true), ...
+            cgg_getOutput({x,y},UnPackFunction,2,'MultiInput',true)), ...
+            TrialFilter,TrialFilter_Value);
+        % RowNames = arrayfun(@(x,y) cgg_getSplitTableRowNames(...
+        %     UnPackFunction(TrialFilter(x,:),y)),(1:size(TrialFilter,1))',...
+        %     TrialFilter_Value);
     case 'Attention'
         RowNames = PassTable.TargetFilter;
 end
@@ -41,9 +55,39 @@ AdditionTable =  table('Size',[NumEntries,NumTableVariables],...
 %%
 TableVariables = [["Accuracy", "cell"]; ...
     ["Window Accuracy", "cell"]; ...
-    ["Area Names", "cell"]];
+    ["Area Names", "cell"]; ...
+    ["Removal Counts", "table"]];
 
 NumTableVariables = size(TableVariables,1);
+%%
+if isempty(Areas)
+AllUniqueAreas = [];
+if strcmp(TableType,'Full')
+
+    for eidx = 1:NumEntries
+        this_PassEntry = PassTable(eidx,:);
+        [IAPathName,~,~] = cgg_generateImportanceAnalysisFileNames(...
+            this_PassEntry,cfg_Epoch);
+        
+        IAPathNameNameExt = string(IAPathName) + ".mat";
+        IAPathNameNameExt = replace(IAPathNameNameExt,"Fold_0","*");
+        IADirectory = dir(IAPathNameNameExt);
+        IAPathNameNameExts = fullfile({IADirectory.folder},{IADirectory.name});
+        if ~isempty(IAPathNameNameExts)
+        IA_Table = load(IAPathNameNameExts{1});
+        IA_Table = IA_Table.IA_Table;
+        this_AreaNames = IA_Table.("AreaNames");
+        
+        [~,MaxIDX] = max(cellfun(@numel, this_AreaNames));
+        AllAreaRemovals = this_AreaNames{MaxIDX};
+        this_AllUniqueAreas = unique(AllAreaRemovals);
+        AllUniqueAreas = unique([AllUniqueAreas,string(this_AllUniqueAreas)]);
+        end
+    end
+end
+else
+AllUniqueAreas = Areas;
+end
 %%
 
 % if NumEntries > 1
@@ -79,10 +123,23 @@ IA_Table_Addition = table('Size',[this_NumRows,NumTableVariables],...
 	    'VariableTypes', TableVariables(:,2));
 end
 
-this_AreaNames = cellfun(@(x) unique(x),this_AreaNames,"UniformOutput",false);
+this_UniqueAreaNames = cellfun(@(x) unique(x),this_AreaNames,"UniformOutput",false);
+
+% [~,MaxIDX] = max(cellfun(@numel, this_AreaNames));
+% AllAreaRemovals = this_AreaNames{MaxIDX};
+% AllUniqueAreas = unique(AllAreaRemovals);
+
+NumberRemovedTable = table();
+for aidx = 1:length(AllUniqueAreas)
+    this_AreaName = AllUniqueAreas{aidx};
+    this_RemovalCount = cellfun(@(x) sum(ismember(x,this_AreaName)),this_AreaNames,"UniformOutput",true);
+    NumberRemovedTable.(this_AreaName) = this_RemovalCount;
+end
+
+IA_Table_Addition.("Removal Counts") = NumberRemovedTable;
 
 if all(cellfun(@isempty,IA_Table_Addition.("Area Names")))
-IA_Table_Addition.("Area Names") = this_AreaNames;
+IA_Table_Addition.("Area Names") = this_UniqueAreaNames;
 else
 % TODO: Add a check to make sure that the order is the same across the
 % different folds. Currently assuming the removal of areas order is the
@@ -101,6 +158,6 @@ end
 AdditionTable{eidx,"Block"} = {IA_Table_Addition};
 AdditionTable.Properties.RowNames{eidx} = this_PassEntry.Properties.RowNames{1};
 end
-
+%%
 AccuracyTable = [AccuracyTable,AdditionTable];
 end
