3a99ad1803087104d5b96baf9b9cb6e81428520b
gerritcg <charles.g.gerrity@vanderbilt.edu>
2026-01-23 16:04:53 -0800

    Introducing handles for updating parameters more easily

---
+++ Processing_Functions_cgg/Input_Output_Information/cgg_generateLoadDataArrayParameter.m
@@ -0,0 +1,9 @@
+classdef cgg_generateLoadDataArrayParameter < handle
+    properties
+        STDChannelOffset = NaN;
+        STDWhiteNoise = NaN;
+        STDRandomWalk = NaN;
+        STDTimeShift = NaN;
+        WantSeparateTimeShift=false;
+    end
+end
